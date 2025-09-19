e1bcc5ad811c61cc147b7543605ddf33f0c356b6
benjamacias <74431154+benjamacias@users.noreply.github.com>
2025-09-19 16:19:15 +0000

    Integrate daily AFIP WSAA token refresh on startup

--- invoices/apps.py
+++ invoices/apps.py
@@ -1,6 +1,23 @@
+import os
+import sys
+
 from django.apps import AppConfig
 
 
 class InvoicesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "invoices"
+
+    def ready(self) -> None:  # pragma: no cover - startup hook
+        super().ready()
+
+        if "runserver" not in sys.argv:
+            return
+
+        run_main = os.environ.get("RUN_MAIN")
+        if run_main != "true" and "--noreload" not in sys.argv:
+            return
+
+        from .afip import refresh_wsaa_token_if_needed
+
+        refresh_wsaa_token_if_needed()
